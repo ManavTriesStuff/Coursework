604fa207ccb5a4ebcb41ff85e1df92de0df1a2f3
Manav <me.manavvarma@gmail.com>
2021-09-18 18:56:30 +0530

    Delete kaggleTest.py

--- Kaggle/kaggleTest.py
+++
@@ -1,5 +0,0 @@
-# Run the code to save predictions in the format used for competition scoring
-
-output = pd.DataFrame({'Id': test_data.Id,
-                       'SalePrice': test_preds})
-output.to_csv('submission.csv', index=False)
